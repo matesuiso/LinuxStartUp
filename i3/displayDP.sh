75422c538f5876b8a82f2fc8cebd8ff0e8a60c5e
matesuiso <matesuiso@hotmail.com>
2020-10-11 18:56:19 -0300

    fix no bg and nvidia defaults

--- i3/displayDP.sh
+++ i3/displayDP.sh
@@ -4,6 +4,7 @@ RES= `xrandr -q | grep \'DP-0\ connected\'`
 
 if [ "$RES" == "" ]; then
 	xrandr --output DP-0 --auto --left-of eDP-1-1 -r 144
+	nvidia-settings --assign CurrentMetaMode="1920x1080_144 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline = On }"
 else
 	xrandr --output DP-0 --off
 fi
